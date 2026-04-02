926b6a8dc1e84f7844c15c869d38f2c48191052a
kshakilrana2025-wq <kshakilrana2025@gmail.com>
2026-04-03 01:54:52 +0600

    Create layout.tsx

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,10 @@
+import Sidebar from '@/components/ui/Sidebar';
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-1 p-8">{children}</main>
+    </div>
+  );
+}
